f53d905cd80b403e6527bc53653ed5979ddb5317
V-O-N-hope <83778394+V-O-N-hope@users.noreply.github.com>
2021-11-02 20:11:38 +0300

    Revert "Eviv"

--- src/main/java/com/bestgroup/calendar/HelloApplication.java
+++ src/main/java/com/bestgroup/calendar/HelloApplication.java
@@ -7,31 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * Main class
- * @version 1.0
- */
 public class HelloApplication extends Application {
-    /**
-     *
-     * @param stage
-     * @throws IOException
-     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500.0D, 600.0D);
-        stage.setTitle("Calendar SPS");
+        Scene scene = new Scene(fxmlLoader.load(), 400.0D, 600.0D);
+        stage.setTitle("Попытка");
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * Program is launched here
-     * @param args
-     */
     public static void main(String[] args) {
         launch();
     }
-
 }
--- src/main/java/com/bestgroup/calendar/HelloController.java
+++ src/main/java/com/bestgroup/calendar/HelloController.java
@@ -1,40 +1,14 @@
 package com.bestgroup.calendar;
 
-import java.net.URL;
-
-import java.util.ResourceBundle;
-
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
+import javafx.scene.control.Label;
 
 public class HelloController {
+    @FXML
+    private Label welcomeText;
 
     @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Text Time;
-
-    @FXML
-    void initialize() {
-        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
-            CurrentTime currentTime = new CurrentTime();
-            Time.setText(currentTime.getCurrentTime());
-        }),
-                new KeyFrame(Duration.seconds(1))
-        );
-        clock.setCycleCount(Animation.INDEFINITE);
-        clock.play();
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
-
-
-
