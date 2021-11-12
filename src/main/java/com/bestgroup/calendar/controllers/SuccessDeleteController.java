8fc0c61a29bf7b2015431b5026669fe225d8bd07
eviv2206 <zhenya.saprin@mail.ru>
2021-11-12 11:30:43 +0300

    create SuccessDelete

---
+++ src/main/java/com/bestgroup/calendar/controllers/SuccessDeleteController.java
@@ -0,0 +1,32 @@
+package com.bestgroup.calendar.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class SuccessDeleteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonOk;
+
+    @FXML
+    private Text text;
+
+    @FXML
+    void initialize() {
+        NewScene nw = new NewScene();
+        text.setText("Успех! Событие удалено.");
+        buttonOk.setOnMouseClicked((event) -> {
+            nw.closeScene(buttonOk);
+            nw.openNewScene("/com/bestgroup/calendar/hello-view.fxml");
+        });
+    }
+}
