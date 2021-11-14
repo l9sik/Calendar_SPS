da14eb41b7aa2899701126c7cce62e6f773c4eb2
l9sik <hotspot.by@gmail.com>
2021-11-14 22:37:39 +0300

    Now you can add event by writeIntoExcel method

--- src/main/java/com/bestgroup/calendar/EventHelper.java
+++ src/main/java/com/bestgroup/calendar/EventHelper.java
@@ -1,6 +1,18 @@
 package com.bestgroup.calendar;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.Row;
+
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Scanner;
 
 public class EventHelper {
@@ -18,7 +30,7 @@ public class EventHelper {
     public void setDescription (String ds){ description = ds;}
     public String getDescription (){ return description; }
 
-    public Boolean isEventExist() {
+    public boolean isEventExist() {
         boolean isExist = false;
         File file = new File("Events.txt");
         Scanner scanner = null;
@@ -36,7 +48,6 @@ public class EventHelper {
         scanner.close();
         return isExist;
     }
-
     public void getEvent(){
         File file = new File("Events.txt");
         Scanner scanner = null;
@@ -54,5 +65,38 @@ public class EventHelper {
             }
         }
         scanner.close();
+    }
+    private void writeIntoExcel(Date eDate, String eTheme, String eDescription, String eTimeNotification){
+        try {
+            File log = new File("Events.xls");
+            FileInputStream file = new FileInputStream(log);
+            HSSFWorkbook wb = new HSSFWorkbook(file);
+            HSSFSheet sheet = wb.getSheetAt(wb.getActiveSheetIndex());
+            Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+
+            Cell date = row.createCell(0);
+            DataFormat format = wb.createDataFormat();
+            CellStyle dateStyle = wb.createCellStyle();
+            dateStyle.setDataFormat(format.getFormat("yyyy-MM-dd"));
+            date.setCellStyle(dateStyle);
+            date.setCellValue(eDate);
+            sheet.autoSizeColumn(0);
+
+            Cell theme = row.createCell(1);
+            theme.setCellValue(eTheme);
+
+            Cell description = row.createCell(2);
+            description.setCellValue(eDescription);
+
+            Cell time = row.createCell(3);
+            time.setCellValue(eTimeNotification);
+
+            FileOutputStream out = new FileOutputStream(log);
+            wb.write(out);
+            out.close();
+            wb.close();
+        }catch (IOException e){
+            System.out.println("Something wrong with excel file");
+        }
     }
 }
