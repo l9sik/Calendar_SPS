fcc3144ffe0a7136b23cfde39c4c4896946d3d7c
Nikita <retdarw@gmail.com>
2021-11-24 15:25:09 +0300

    changes

--- src/main/java/com/bestgroup/calendar/controllers/EventsController.java
+++ src/main/java/com/bestgroup/calendar/controllers/EventsController.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
  *
  * @author l9sik
  */
+
+
 public class EventsController {
 
     @FXML
---
+++ src/test/java/com/bestgroup/calendar/controllers/EventsControllerTest.java
@@ -0,0 +1,16 @@
+package com.bestgroup.calendar.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventsControllerTest {
+
+	@org.junit.jupiter.api.Test
+	void initialize() {
+		assert (true);
+	}
+
+	@org.junit.jupiter.api.Test
+	void getList() {
+		assert (true);
+	}
+}
