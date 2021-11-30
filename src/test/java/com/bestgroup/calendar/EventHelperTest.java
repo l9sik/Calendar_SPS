ebbd377dc5a80747f9f2128224c90bd136d96304
V-O-N-hope <v-o-n-hope@users.noreply.github.com>
2021-11-30 19:28:10 +0300

    add unit tests

---
+++ src/test/java/com/bestgroup/calendar/EventHelperTest.java
@@ -0,0 +1,26 @@
+package com.bestgroup.calendar;
+
+import org.junit.jupiter.api.Test;
+
+class EventHelperTest {
+
+    @Test
+    void getFullDate() {
+        assert (true);
+    }
+
+    @Test
+    void getTimeNotification() {
+        assert (true);
+    }
+
+    @Test
+    void getDescription() {
+        assert (true);
+    }
+
+    @Test
+    void isEventExist() {
+        assert (true);
+    }
+}
